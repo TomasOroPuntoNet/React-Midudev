a14c98a0b4cc35298bd0a9462d5ad2a4e8cde2c8
Tomás Aberastain Oro <tomasao87@gmail.com>
2023-05-29 09:40:25 -0300

    genero una card, con estilado (twitter) y agrego un estado, que se ve alterado al apretar el botón

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import './App.css'
 import UserCard from './Components/UserCard/UserCard'
 
 function App() {
-
+  
 
   return (
-    <>
-      <UserCard  mail='pepe@maurinho.com' img='willsmith'>Pepardo </UserCard>  
+    <div style={{display:'flex', flexDirection:'column', gap:'10px'}}>
+      <UserCard   isFollowing mail='pepe@maurinho.com' img='willsmith'>Pepardo </UserCard>  
       <UserCard  mail='pepe@maurinho.com' img='Carl'>Pepardol</UserCard>  
-      <UserCard  mail='pepe@maurinho.com' img='willsmith'>Pepardoz</UserCard>  
-    </>
+      <UserCard isFollowing  mail='pepe@maurinho.com' img='willsmith'>Pepardoz</UserCard>  
+    </div>
   )
 }
 
--- src/Components/UserCard/UserCard.jsx
+++ src/Components/UserCard/UserCard.jsx
@@ -1,22 +1,29 @@
-import { Children } from "react"
-
+import { useState } from 'react'
+import './UserCard.css'
 
 const UserCard = ({children, mail, img}) =>{
+    
+    const [isFollowing, setIsFollowing] = useState(true)
+    const handleClick = ()=>{setIsFollowing(!isFollowing)}
+    const text = isFollowing ? 'Seguir' : 'Dejar de seguir'
+    const buttonClassName = isFollowing ? 'user-card-button is-following' : 'user-card-button button'
+
     return(
-        <article style={{display: "flex", justifyContent: 'center', flexDirection:'row', alignItems: 'center', gap:'40px', backgroundColor:"black", padding:'10px', borderRadius:'1rem'}}>
-            <header style={{display:'flex', justifyContent: 'center', gap:'10px'}}>
-                <img style={{height: '4rem', borderRadius:'50%'}}src={`https://unavatar.io/instagram/${img}`}></img>
-                <div style={{display:'flex', flexDirection:'column', alignItems:'flex-start', justifyContent:'center'}}>
-                    <strong>{children}</strong>
-                    <span>{mail}</span>
-                </div>
-            </header>
-            <aside>
-                <button>
-                    Seguir
-                </button>
-            </aside>
-        </article>
+  
+            <article className={buttonClassName}>
+                <header style={{display:'flex', justifyContent: 'center', gap:'10px'}}>
+                    <img style={{height: '4rem', borderRadius:'50%'}}src={`https://unavatar.io/instagram/${img}`}></img>
+                    <div style={{display:'flex', flexDirection:'column', alignItems:'flex-start', justifyContent:'center'}}>
+                        <strong>{children}</strong>
+                        <span>{mail}</span>
+                    </div>
+                </header>
+                <aside>
+                    <button onClick={handleClick}>
+                        {text}
+                    </button>
+                </aside>
+            </article>
     )
 }
 
